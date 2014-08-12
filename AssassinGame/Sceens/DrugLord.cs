6660f67719af2db7e62b1630a058d419ad56d999
Grinsler <reverentrunner@gmail.com>
2014-08-12 14:42:34 -0600

    Added Solution Files

---
+++ AssassinGame/Sceens/DrugLord.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssassinGame.Sceens
+{
+    public class DrugLord
+    {
+        
+        string ConsoleSetup = "";
+        string ConsoleChoice1 = "";
+        string ConsoleChoice2 = "";
+        string ConsoleChoice3 = "";
+
+        string[] PossibleChoices = new string[4];
+
+        public string Property_ConsoleSetup
+        {
+            get
+            {
+                return ConsoleSetup;
+            }
+            set
+            {
+                ConsoleSetup = value;
+            }
+        }
+        public string Property_ConsoleChoice1
+        {
+            get
+            {
+                return ConsoleChoice1;
+            }
+            set
+            {
+                ConsoleChoice1 = value;
+            }
+        }
+        public string Property_ConsoleChoice2
+        {
+            get
+            {
+                return ConsoleChoice2;
+            }
+            set
+            {
+                ConsoleChoice2 = value;
+            }
+        }
+        public string Property_ConsoleChoice2
+        {
+            get
+            {
+                return ConsoleChoice2;
+            }
+            set
+            {
+                ConsoleChoice2 = value;
+            }
+        }
+        public string[] Property_PossibleChoices
+        {
+            get
+            {
+                return PossibleChoices;
+            }
+            private set
+            {
+                PossibleChoices = value;
+            }
+        }
+        
+        
+        public string[] LeavingNeumont()
+        {
+            ConsoleSetup = "You leave HQ through a back enterance, You decide that you can save time by jumping over the dumpster and crossing the construction field. Or take the easy route, and go through the alley, where you notice a shady fellow.";
+            ConsoleChoice1 = "You decide to a risk, and jump over the dumpster";
+            ConsoleChoice2 = "Play it somwhat safe, and take the alley";
+            ConsoleChoice3 = "You're an assassin, and you just spoted a possible intruder - kill him";
+
+            PossibleChoices[0] = ConsoleSetup;
+            PossibleChoices[1] = ConsoleChoice1;
+            PossibleChoices[2] = ConsoleChoice2;
+            PossibleChoices[3] = ConsoleChoice3;
+            
+            return PossibleChoices;
+        }
+    }
+}
---
+++ AssassinGame/Sceens/Mission.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AssassinGame.Sceens
+{
+    public class Mission
+    {
+        string[] Choices;
+
+        public string Property_Choice1 { get; private set; }
+        public string Property_Choice2 { get; private set; }
+        public string Property_Choice3 { get; private set; }
+        public string Property_Setup { get; private set; }
+
+        public Mission (string[] choices)
+	    {
+            Choices = choices;
+	    }
+        public int Interaction1()
+        {
+            int userchoice = 0;
+
+            Property_Setup = Choices[0];
+            Property_Choice1 = Choices[1];
+            Property_Choice2 = Choices[2];
+            Property_Choice3 = Choices[3];
+
+
+            return 1;
+        }
+    }
+}
